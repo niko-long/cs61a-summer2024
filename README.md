952542d96179fb0acd3838f4a08d0acca311f8d9
Xiaolong Song <33993813+niko-long@users.noreply.github.com>
2024-08-09 14:53:47 +0200

    lab00 finished

---
+++ README.md
@@ -0,0 +1,8 @@
+# cs61a-summer2024
+study notes of ucb cs61a-summer2024 Structure and Interpretation of Computer Programs
+
+
+
+**2024/08/09**
+- Finished [lab00](https://cs61a.org/lab/lab00/)  
+- time cost: 5min
---
+++ lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_four()
+    2024
+    """
+    a = 2 * 1000 + 2*100 - 2*88 
+    return a
